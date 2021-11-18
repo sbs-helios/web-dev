47a2b6f7e904395140ad151a9c2214fcc71d904d
Aryan Nath <74306760+natharyan@users.noreply.github.com>
2021-11-18 08:44:11 +0530

    Delete Fall 2021 Workshop/Session Files/Session 1 directory

--- Fall 2021 Workshop/Session Files/Session 1/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction(){
-    x= document.getElementById('text').innerHTML
-    if (x=='Some Text'){
-    document.getElementById('text').innerHTML = 'Text Changed!'
-    }
-    else{
-        document.getElementById('text').innerHTML = 'Some Text'
-    }
-}
